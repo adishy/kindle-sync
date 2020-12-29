bc8caa1562461e544d9fb3b283c78377232ac74d
Aditya Shylesh <adishy@umich.edu>
2020-12-29 15:51:51 +0400

    Initial commit

---
+++ test-sync.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/test', methods = [ 'POST' ])
+def test():
+    body = request.get_json()
+    print("Request:", body)
+    return jsonify({ "status": 200 }) 
+
+if __name__ == '__main__':
+    app.run(port = 8000, host = '0.0.0.0', debug = True)
